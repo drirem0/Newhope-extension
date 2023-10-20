3d95ca8e68db7e60e085782386354a640e4f5622
ComActivity <104488499+ComActivity@users.noreply.github.com>
2023-10-20 13:51:17 +1100

    Add files via upload
    Corrections for XtendM3 feedback

--- src/main/groovy/WKF009/EXT198MI_ProcessVariance.groovy
+++ src/main/groovy/WKF009/EXT198MI_ProcessVariance.groovy
@@ -1,3 +1,25 @@
+/***************************************************************
+ *                                                             *
+ *                           NOTICE                            *
+ *                                                             *
+ *   THIS SOFTWARE IS THE PROPERTY OF AND CONTAINS             *
+ *   CONFIDENTIAL INFORMATION OF INFOR AND/OR ITS AFFILIATES   *
+ *   OR SUBSIDIARIES AND SHALL NOT BE DISCLOSED WITHOUT PRIOR  *
+ *   WRITTEN PERMISSION. LICENSED CUSTOMERS MAY COPY AND       *
+ *   ADAPT THIS SOFTWARE FOR THEIR OWN USE IN ACCORDANCE WITH  *
+ *   THE TERMS OF THEIR SOFTWARE LICENSE AGREEMENT.            *
+ *   ALL OTHER RIGHTS RESERVED.                                *
+ *                                                             *
+ *   (c) COPYRIGHT 2020 INFOR.  ALL RIGHTS RESERVED.           *
+ *   THE WORD AND DESIGN MARKS SET FORTH HEREIN ARE            *
+ *   TRADEMARKS AND/OR REGISTERED TRADEMARKS OF INFOR          *
+ *   AND/OR ITS AFFILIATES AND SUBSIDIARIES. ALL RIGHTS        *
+ *   RESERVED.  ALL OTHER TRADEMARKS LISTED HEREIN ARE         *
+ *   THE PROPERTY OF THEIR RESPECTIVE OWNERS.                  *
+ *                                                             *
+ ***************************************************************
+ */
+ 
  import groovy.lang.Closure;
  import java.time.LocalDate;
  import java.time.LocalDateTime;
@@ -11,7 +33,17 @@
  import java.text.DecimalFormat;
  import java.time.ZoneId;
  
- public class ProcessVariance extends ExtendM3Transaction {
+/*
+* Modification area - M3
+* Nbr               Date      User id     Description
+* WKF009            20230927  KVERCO      Supplier invoice variance recoding
+*/
+ 
+/**
+* ProcessVariance - Check for Supplier Invoice variances and recode to purchase accounting string
+*
+*/
+public class ProcessVariance extends ExtendM3Transaction {
   private final MIAPI mi;
   private final IonAPI ion;
   private final LoggerAPI logger;
